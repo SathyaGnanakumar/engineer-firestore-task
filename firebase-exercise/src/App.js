98f37d01074f00932ec41ff6dbcb297504c3de6f
Sathya <sathya.gnanakumar@gmail.com>
2024-02-14 12:37:37 -0500

    Firebase Task Finished

--- firebase-exercise/src/App.js
+++ firebase-exercise/src/App.js
@@ -1,30 +1,62 @@
-import React, { useState, useEffect } from 'react'; // Or Whatever React imports you want
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import db from './firebase-config'; // Import Firebase Firestore instance
+import { collection, addDoc, deleteDoc, query, getDocs, doc } from "firebase/firestore"; // Import Firestore methods
 
 function App() {
-
   const [newChapterName, setNewChapterName] = useState('');
   const [chapterToDelete, setChapterToDelete] = useState('');
+  const [chapters, setChapters] = useState([]);
 
   const displayDatabase = async () => {
-    // TODO: Implement the logic to fetch and display chapters from the database
+    try {
+      const querySnapshot = await getDocs(collection(db, 'chapters_test'));
+      const chapterData = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data()
+      }));
+      console.log("Chapter data:", chapterData);
+      setChapters(chapterData);
+      console.log(chapters);
+    } catch (error) {
+      console.error('Error fetching chapters:', error);
+    }
   };
+  
+  
 
-  // Function to add a chapter
   const addChapter = async () => {
-    // TODO: Implement the logic to add a new chapter to the database
+    try {
+      const docRef = await addDoc(collection(db, 'chapters_test'), {
+        name: newChapterName
+      });
+      displayDatabase();
+      setNewChapterName('');
+    } catch (error) {
+        console.error('Error adding chapter:', error);
+    }
   };
 
-  // Function to delete a chapter
   const deleteChapter = async () => {
-    // TODO: Implement the logic to delete a chapter from the database
+    try {
+      await deleteDoc(doc(db, 'chapters_test', chapterToDelete));
+      displayDatabase();
+      setChapterToDelete('');
+    } catch (error) {
+        console.error('Error deleting chapter:', error);
+    }
   };
 
   return (
     <div className="App">
       <h1>Chapter Operations</h1>
       <button onClick={displayDatabase}>Display Database</button>
+      <div>
+        <ul>
+          {chapters.map((chapter, index) => (
+            <li key={index}>{chapter.id}</li>
+          ))}
+        </ul>
+      </div>
       <div>
         <input
           type="text"
